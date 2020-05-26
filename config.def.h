dd302323a07ff63ecfe282302a6c8b1586ce8c54
Sajjad H Rizvi <mr.sajjad.rizvi@gmail.com>
2020-05-26 23:20:53 +0300

    font size & border increase

--- config.def.h
+++ config.def.h
@@ -1,16 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-static const char *fonts[]            = {"Mononoki Nerd Font:size=10:antialias=true:autohint=true",
-                                         "Hack:size=10:antialias=true:autohint=true",
-                                         "JoyPixels:size=10:antialias=true:autohint=true"
+static const char *fonts[]            = {"Mononoki Nerd Font:size=12:antialias=true:autohint=true",
+                                         "Hack:size=12:antialias=true:autohint=true",
+                                         "JoyPixels:size=12:antialias=true:autohint=true"
 																				};
-static const char dmenufont[]       = "Hack:size=10";
+static const char dmenufont[]       = "Hack:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
