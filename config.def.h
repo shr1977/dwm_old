f900febf3ee958ac68bb607c4e0afb38973c8521
Sajjad H Rizvi <mr.sajjad.rizvi@gmail.com>
2021-02-12 19:51:13 +0300

    committing changes

--- config.def.h
+++ config.def.h
@@ -63,13 +63,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-<<<<<<< HEAD
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "konsole, NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
-=======
-static const char *termcmd[]  = { "konsole", NULL };
->>>>>>> config
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
